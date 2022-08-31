3b466e1b6d0842eace7bc8a31ad8b98247a90ad8
Ihsan Caliskanoglu <ihsan.caliskanoglu@gmail.com>
2022-08-31 17:13:38 +0300

    postgresql bechmark scripts

--- write-benchmark.sh
+++ write-benchmark.sh
@@ -1,7 +1,7 @@
-export BENCHMARK_HOST=
+export BENCHMARK_HOST=$DB_HOST
+export PGPASSWORD=$DB_PASSWORD
 export BENCHMARK_PORT=5432
 export BENCHMARK_DB=ihsan
-export PGPASSWORD=
 export BENCHMARK_USER=postgres
 # pgbench control environment variables
 export BENCHMARK_CONNECTIONS=16
@@ -9,5 +9,7 @@ export BENCHMARK_THREADS=8
 export BENCHMARK_SQL_FILE=write-benchmark.sql
 # time in seconds to run test
 export BENCHMARK_TIME=300
+
+psql -h $BENCHMARK_HOST -p $BENCHMARK_PORT -f create-database-if-not-exist.sql -U $BENCHMARK_USER
 
 pgbench -c $BENCHMARK_CONNECTIONS -j $BENCHMARK_THREADS -T $BENCHMARK_TIME -U $BENCHMARK_USER -d $BENCHMARK_DB -h $BENCHMARK_HOST -p $BENCHMARK_PORT -f $BENCHMARK_SQL_FILE
